7d0300394041746eb90e0d615eff0b7715492636
gabriellgomess <51470523+gabriellgomess@users.noreply.github.com>
2021-11-30 11:00:21 -0300

    inclusão dos arquivos

---
+++ script.js
@@ -0,0 +1,32 @@
+
+// $(document).ready(function(){
+  // FUNCIONA ###############
+
+  // $("#myBtn").click(function(){
+  //     $("#myDropdown").toggle();
+  // });
+
+  // TAMBÉM FUNCIONA ###############
+
+  // $("#btn").click(function(){
+  //   $("#myDropdown").toggleClass("show");
+  // })
+
+  // FUNCIONA COM SLIDETOGGLE ###############
+
+  $("#myBtn").click(function(){
+    $("#myDropdown").slideToggle();
+  }) 
+
+// });
+
+
+
+$("#criadiv").click(function(){
+  $('#root').append('<div></div>');
+    let cont = $("#root > div").length
+    var element = $("#root").find("div")
+    console.log( element[0] )
+  $("#resposta").html("+"+cont)
+})
+ 
